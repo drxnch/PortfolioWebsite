5ab879dea3fd0f1e423d1f9ad4598073fa23c285
dran794 <deven.ranchhod3@gmail.com>
2025-12-29 19:10:24 +1300

    added modals
    also added projects into cards. Changed colour scheme

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,44 @@
 const softwareProjectButton = document.getElementById("softwareProjectButton");
 const firmwareProjectButton = document.getElementById("firmwareProjectButton");
+const hardwareProjectButton = document.getElementById("hardwareProjectButton");
+const fpgaProjectButton = document.getElementById("fpgaProjectButton");
+
 const softwareProjects = document.getElementById("softwareProjects");
 const firmwareProjects = document.getElementById("firmwareProjects");
+const hardwareProjects = document.getElementById("hardwareProjects");
+const fpgaProjects = document.getElementById("fpgaProjects");
 
-// function showSoftwareProjects() {
-//   var x = document.getElementById("softwareProjects");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
-
-// function showSoftwareProjects() {
-//   const x = document.getElementById("softwareProjects");
-//   const isVisible = window.getComputedStyle(x).display === "none";
-
-//   x.style.display = isVisible ? "block" : "none";
-// }
-
+const flappyBirdFpga = document.getElementById("flappyBirdFpga");
 function showSoftwareProjects() {
   softwareProjects.classList.remove("hidden");
   firmwareProjects.classList.add("hidden");
+  hardwareProjects.classList.add("hidden");
+  fpgaProjects.classList.add("hidden");
 }
 
 function showFirmwareProjects() {
   softwareProjects.classList.add("hidden");
   firmwareProjects.classList.remove("hidden");
+  hardwareProjects.classList.add("hidden");
+  fpgaProjects.classList.add("hidden");
 }
+
+function showHardwareProjects() {
+  softwareProjects.classList.add("hidden");
+  firmwareProjects.classList.add("hidden");
+  hardwareProjects.classList.remove("hidden");
+  fpgaProjects.classList.add("hidden");
+}
+
+function showFpgaProjects() {
+  softwareProjects.classList.add("hidden");
+  firmwareProjects.classList.add("hidden");
+  hardwareProjects.classList.add("hidden");
+  fpgaProjects.classList.remove("hidden");
+}
+
+const flappyBirdDialog = document.getElementById("flappyBirdDialog");
+
+flappyBirdFpga.addEventListener("click", () => {
+  flappyBirdDialog.showModal();
+});
